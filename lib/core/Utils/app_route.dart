4f1d686ab79f862ea1979ee287cc2962bd4c681d
Abdullah Abed <abedabdallah834@gmail.com>
2024-09-03 16:23:24 +0300

    update in file app route

--- lib/core/Utils/app_route.dart
+++ lib/core/Utils/app_route.dart
@@ -1,5 +1,8 @@
+import 'package:carwash/Features/home/presentation/views/about_us_view.dart';
 import 'package:carwash/Features/home/presentation/views/home_view.dart';
 import 'package:carwash/Features/home/presentation/views/widgets/home_nav_bar_widget.dart';
+import 'package:carwash/Features/profile/presentation/views/profile_view.dart';
+import 'package:carwash/Features/services/presentation/views/services_view.dart';
 import 'package:flutter/material.dart';
 
 abstract class AppRoute {
@@ -18,13 +21,13 @@ abstract class AppRoute {
         return MaterialPageRoute(builder: (context) => const HomeView());
 
       case aboutUs:
-        return MaterialPageRoute(builder: (context) => const HomeView());
+        return MaterialPageRoute(builder: (context) => const AboutUsView());
 
       case profile:
-        return MaterialPageRoute(builder: (context) => const HomeView());
+        return MaterialPageRoute(builder: (context) => const ProfileView());
 
       case service:
-        return MaterialPageRoute(builder: (context) => const HomeView());
+        return MaterialPageRoute(builder: (context) => const ServicesView());
       default:
         return MaterialPageRoute(builder: (context) => const Scaffold());
     }
