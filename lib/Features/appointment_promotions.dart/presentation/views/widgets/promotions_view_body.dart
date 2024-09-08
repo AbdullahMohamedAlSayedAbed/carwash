ffc65439a68c53ef6adac275bdf8c1f80def9e5f
Abdullah Abed <abedabdallah834@gmail.com>
2024-09-08 12:21:03 +0300

    create promotions view

--- lib/Features/appointment_promotions.dart/presentation/views/widgets/promotions_view_body.dart
+++ lib/Features/appointment_promotions.dart/presentation/views/widgets/promotions_view_body.dart
@@ -5,6 +5,11 @@ class PromotionsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Promotions'),
+      ),
+      body: Container(),
+    );
   }
 }
